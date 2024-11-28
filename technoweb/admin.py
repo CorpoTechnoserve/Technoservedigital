5d9f65319101abe99b34f2b95f13fc84388b87eb
nidhi2931 <imnidhi.gautam8690@gmail.com>
2024-11-29 00:09:26 +0530

    new updates

--- technoweb/admin.py
+++ technoweb/admin.py
@@ -3,29 +3,29 @@ from .models import *
 from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
-# admin.site.register(Contact)
-# admin.site.register(HrCorner)
-# admin.site.register(AskQuery)
-# admin.site.register(SoftwareDevelopment)
-@admin.register(StudentCorner)
-class StudentCorner(ImportExportModelAdmin):
-    pass
+admin.site.register(Contact)
+admin.site.register(HrCorner)
+admin.site.register(AskQuery)
+admin.site.register(SoftwareDevelopment)
+# @admin.register(StudentCorner)
+# class StudentCorner(ImportExportModelAdmin):
+#     pass
 
-@admin.register(HrCorner)
-class HrCorner(ImportExportModelAdmin):
-    pass
+# @admin.register(HrCorner)
+# class HrCorner(ImportExportModelAdmin):
+#     pass
 
-@admin.register(Contact)
-class Contact(ImportExportModelAdmin):
-    pass
+# @admin.register(Contact)
+# class Contact(ImportExportModelAdmin):
+#     pass
 
-@admin.register(AskQuery)
-class AskQuery(ImportExportModelAdmin):
-    pass
+# @admin.register(AskQuery)
+# class AskQuery(ImportExportModelAdmin):
+#     pass
 
-@admin.register(SoftwareDevelopment)
-class SoftwareDevelopment(ImportExportModelAdmin):
-    pass
+# @admin.register(SoftwareDevelopment)
+# class SoftwareDevelopment(ImportExportModelAdmin):
+#     pass
 
 
 
